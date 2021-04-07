ad251259efbb6653cd7bf2aa4af7088e3ccb9609
Yetunde Dada <yetudada@users.noreply.github.com>
2021-04-07 01:00:31 +0100

    Update shell script

--- get-started/02_set_up_data/init.sh
+++ get-started/02_set_up_data/init.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-echo "This background script sets up your Kedro project"
+# Tell users that they must wait until Kedro is installed and configured
+echo "Please wait until Kedro is installed and configured."
 
+# Install Kedro
 pip install kedro
 
+# Show Kedro version
+kedro info
+
+# Create a new Kedro project called "Get Started"
 kedro new --config config.yml
 
+# Change the project directory
+cd get-started
+
+# Install project dependencies
 kedro install
 
-echo "done" >> /opt/.backgroundfinished
+# Print out done
+echo "done"
+
+# Clear screen
+clear
+
