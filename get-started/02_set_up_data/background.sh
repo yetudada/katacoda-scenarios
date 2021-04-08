b479faeff9c2d0e65872cbf51fa096a562dc58e2
Yetunde Dada <yetudada@users.noreply.github.com>
2021-04-08 15:19:25 +0100

    Tweak workflow for user waiting

--- get-started/02_set_up_data/background.sh
+++ get-started/02_set_up_data/background.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Tell users that they must wait until Kedro is installed and configured
-echo "Please wait until Kedro is installed and configured."
+echo "Wait until Kedro is installed and configured."
 
 # Install Kedro
 pip install kedro
-
-# Show Kedro version
-kedro info
 
 # Create a new Kedro project called "Get Started"
 kedro new --config "./02_set_up_data/config.yml"
--- get-started/02_set_up_data/foreground.sh
+++ get-started/02_set_up_data/foreground.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-
-echo "Waiting to finish setting up your Kedro project"; while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done;
-echo "Done"
+echo "Waiting to complete"; while [ ! -f /opt/.backgroundfinished ]; do sleep 2; done; echo "Done"
